26c1a17b36a8a3fa75142270a5c19238660e72f5
sfiala <shereefiala@gmail.com>
2018-11-03 21:32:06 +1100

    Old project import

---
+++ Assets/Scripts/LevelMovement.cs
@@ -0,0 +1,31 @@
+﻿/*
+* Copyright (c) Dylan Faith (Whipflash191)
+* https://twitter.com/Whipflash191
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMovement : MonoBehaviour {
+    public Vector3 mvmntDirection;
+    [Range(0, 100)]
+    public float mvmntScale;
+    [Range(0, 100)]
+    public float maxSpeed;
+    public float currentSpeed;
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(currentSpeed < maxSpeed)
+        {
+            currentSpeed += Mathf.Lerp(0, maxSpeed, (Time.deltaTime * mvmntScale));
+        }
+        gameObject.transform.Translate((mvmntDirection * Time.deltaTime * currentSpeed));
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    public bool canJump = false;
+    bool jump;
+    Rigidbody2D rb;
+    public float jumpForce = 0;
+    [Range(0, 1000)]
+    public float maxJumpHeight;
+    public float currentMaxJumpHeight;
+    [Range(0, 100)]
+    public float climbRate;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        currentMaxJumpHeight = maxJumpHeight;
+	}
+
+    // Update is called once per frame
+    private void Update()
+    { 
+        if (Input.touchCount > 0)
+        {
+            jump = true;
+        } else
+        {
+            jump = false;
+        }
+    }
+    void FixedUpdate ()
+    {
+        if (jump)
+        {
+            if (currentMaxJumpHeight > 0)
+            {
+                currentMaxJumpHeight -= climbRate;
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "floor")
+        {
+            currentMaxJumpHeight = maxJumpHeight;
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "floor")
+        {
+            canJump = false;
+        }
+    }
+}
