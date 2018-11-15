e95bcf9eb4d69ee1d059bdfa01dcda7651d9cfd2
Dylan Faith <whipflash191@gmail.com>
2018-11-15 14:01:48 +1100

    Tile Disable Off Screen
    First draft of the tile disable script

--- Assets/Scripts/LevelMovement.cs
+++ Assets/Scripts/LevelMovement.cs
@@ -7,7 +7,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelMovement : MonoBehaviour {
+public class LevelMovement : MonoBehaviour
+{
+    //Level Movement
     public Vector3 mvmntDirection;
     [Range(0, 100)]
     public float mvmntScale;
@@ -15,14 +17,36 @@ public class LevelMovement : MonoBehaviour {
     public float maxSpeed;
     public float currentSpeed;
 
+    //Off Screen Checks
+    public float orthSize;
+    public float aspRatio;
+    public float gameWidth = 0;
+    public float gameHeight = 0;
+    public List<GameObject> tiles = new List<GameObject>();
+
     private void Start()
     {
+        orthSize = Camera.main.orthographicSize;
+        aspRatio = Camera.main.aspect;
+        gameHeight = orthSize * 2;
+        gameWidth = gameHeight * aspRatio;
+    }
 
+    private void Update()
+    {
+        foreach (GameObject item in tiles)
+        {
+            if (Vector2.Distance(item.transform.position, new Vector2(0, 0)) > (gameWidth / 2 + 0.5f))
+            {
+                print(item.transform.name + " " + Vector2.Distance(item.transform.position, new Vector2(0, 0)));
+                item.GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        if(currentSpeed < maxSpeed)
+        if (currentSpeed < maxSpeed)
         {
             currentSpeed += Mathf.Lerp(0, maxSpeed, (Time.deltaTime * mvmntScale));
         }
